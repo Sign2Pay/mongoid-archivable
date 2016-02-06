75c276c4ce9f9ef6155500dfc9879973ce5fdea6
Tomas Celizna <tomas.celizna@gmail.com>
2016-02-06 10:52:29 +0100

    code cleanup

--- lib/mongoid/archivable.rb
+++ lib/mongoid/archivable.rb
@@ -2,7 +2,6 @@ require 'active_support/concern'
 
 module Mongoid
   module Archivable
-
     extend ActiveSupport::Concern
 
     module Restoration
@@ -13,8 +12,9 @@ module Mongoid
       end
 
       def original_document
-        self.original_class_name.constantize.new(attributes.except("_id", "original_id", "original_type", "archived_at")) do |doc|
-          doc.id = self.original_id
+        excluded_attributes = %i(_id original_id original_type archived_at)
+        original_class_name.constantize.new(attributes.except(*excluded_attributes)) do |doc|
+          doc.id = original_id
         end
       end
 
@@ -22,8 +22,8 @@ module Mongoid
       # since previous versions of this gem did not use this field, fall back
       # to previous method -- removing the '::Archive' from archive class name
       def original_class_name
-        if self.respond_to?(:original_type) && self.original_type.present? # gem version >= 1.3.0, stored as a field.
-          self.original_type
+        if respond_to?(:original_type) && original_type.present? # gem version >= 1.3.0, stored as a field.
+          original_type
         else
           self.class.to_s.gsub(/::Archive\z/, '') # gem version < 1.3.0, turns "User::Archive" into "User".
         end
@@ -31,11 +31,12 @@ module Mongoid
     end
 
     included do
-      self.const_set("Archive", Class.new)
-      self.const_get("Archive").class_eval do
+      const_set('Archive', Class.new)
+      const_get('Archive').class_eval do
         include Mongoid::Document
         include Mongoid::Attributes::Dynamic
         include Mongoid::Archivable::Restoration
+        
         field :archived_at, type: Time
         field :original_id, type: String
         field :original_type, type: String
@@ -47,12 +48,11 @@ module Mongoid
     private
 
     def archive
-      self.class.const_get("Archive").create(attributes.except("_id", "_type")) do |doc|
-        doc.original_id = self.id
+      self.class.const_get('Archive').create(attributes.except('_id', '_type')) do |doc|
+        doc.original_id = id
         doc.original_type = self.class.to_s
         doc.archived_at = Time.now.utc
       end
     end
-
   end
 end
